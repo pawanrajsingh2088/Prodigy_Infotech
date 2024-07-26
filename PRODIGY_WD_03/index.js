a5a4bd3d776b13c8171ba7ecfdff64f35bbc7207
Pawan Raj Singh <122471520+pawanrajsingh2088@users.noreply.github.com>
2024-07-26 21:13:08 +0530

    Update index.js

--- PRODIGY_WD_03/index.js
+++ PRODIGY_WD_03/index.js
@@ -22,7 +22,7 @@ btn.addEventListener("click", () => {
 })
 
 function check_win() {
-    if (moves <= 9) {
+    if (moves < 9) {
         if ((arr[0][0] === "X" && arr[0][1] === "X" && arr[0][2] === "X") || (arr[1][0] === "X" && arr[1][1] === "X" && arr[1][2] === "X") || (arr[2][0] === "X" && arr[2][1] === "X" && arr[2][2] === "X") || (arr[0][0] === "X" && arr[1][0] === "X" && arr[2][0] === "X") || (arr[0][1] === "X" && arr[1][1] === "X" && arr[2][1] === "X") || (arr[0][2] === "X" && arr[1][2] === "X" && arr[2][2] === "X") || (arr[0][0] === "X" && arr[1][1] === "X" && arr[2][2] === "X") || (arr[0][2] === "X" && arr[1][1] === "X" && arr[2][0] === "X")) {
             win.innerHTML = "<------X Wins the Game-------->";
             btn.style.display = "block";
